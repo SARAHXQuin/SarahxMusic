b4bc397afd9b70c7979a7e4a25408d92dafda17d
SARAHXQuin <136741230+SARAHXQuin@users.noreply.github.com>
2024-07-17 17:01:18 +0530

    Update skip.py

--- sarahxmusic/plugins/admins/skip.py
+++ sarahxmusic/plugins/admins/skip.py
@@ -148,7 +148,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, chat_id)
-        img = await get_thumb(videoid, user_id)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -218,7 +218,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, chat_id)
-            img = await get_thumb(videoid, chat_id)
+            img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
