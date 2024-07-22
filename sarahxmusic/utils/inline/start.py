505aa4fc611d8c64d11b8eaeb6a6cbdcc9ade02c
SARAHXQuin <136741230+SARAHXQuin@users.noreply.github.com>
2024-07-22 23:31:05 +0530

    oii

--- sarahxmusic/utils/inline/start.py
+++ sarahxmusic/utils/inline/start.py
@@ -20,18 +20,18 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✚ 𝐀ᴅᴅ 𝐌ᴇ 𝐈ɴ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ ✚",
+                text=" 𝐀ᴅᴅ 𝐌ᴇ 𝐈ɴ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ ",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text="🤔 𝐇ᴇʟᴘ 🤔", callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=" 𝐇ᴇʟᴘ 𝐀ɴᴅ 𝐂ᴏᴍᴍᴀɴᴅꜱ ", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="🌟 𝐆ʀᴏᴜᴘ 🌟", url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text="💌 𝐂ʜᴀɴɴᴇʟ 💌", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=" 𝐍ᴇᴛᴡᴏʀᴋ ", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=" 𝐂ʜᴀɴɴᴇʟ ", url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="🥀 𝐎ᴡɴᴇʀ 🥀", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="❤️‍🔥 𝐒ᴏᴜʀᴄᴇ ❤️‍🔥", url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=" 𝐌ᴏᴍ ", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=" 𝐒ᴏᴜʀᴄᴇ ", url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
